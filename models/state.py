f3126369ead922e0cbccf9edf8015a0b00e3f402
kael1706 <cdcp-1795@hotmail.com>
2020-04-01 01:22:29 +0000

    task 6. updeted class city, state, base_model, db_storage and init module

--- models/state.py
+++ models/state.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship(
+            'City',
+            cascade='all, delete-orphan',
+            backref='state',
+        )
+    else:
+        @property
+        def cities(self):
+            mycities = []
+            for id, c in models.storage.all(City).item():
+                if self.id == c.state.id:
+                    mycities.append(c)
+            return mycities
