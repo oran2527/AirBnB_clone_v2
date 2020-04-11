f3673d858cb8ceb548353431a3c8f7c4c6191a28
oran2527 <orlago250183@gmail.com>
2020-04-11 15:02:22 +0000

    project deploy point 2 deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python3
-''' point 2 deploy '''
+''' point 1 create tgz '''
 import os
-from fabric.api import *
+import os.path
+import datetime
+from fabric.api import local, env, put, run
 ''' os.paht = routes to directories and files, create folders '''
 ''' fabric.api = create tgz files '''
 ''' datetime = in order to get current date and time '''
+
+
+env.hosts = ['35.231.52.206', '54.226.104.83']
+env.password = 'Ironmaiden2*'
+
+
+def do_pack():
+    ''' function to pack in tgz file '''
+    if not os.path.exists('./versions'):
+        os.mkdir('versions')
+    if os.path.exists('./web_static'):
+        now = datetime.datetime.now()
+        b = "web_static_"
+        ny = now.year
+        nm = now.month
+        nd = now.day
+        nh = now.hour
+        nmi = now.minute
+        ns = now.second
+        e = ".tgz"
+        n = "{}{}{}{}{}{}{}{}".format(b, ny, nm, nd, nh, nmi, ns, e)
+        tgzfile = local('tar -cvzf versions/{} web_static'.format(n))
+        if tgzfile.failed:
+            return None
+        return 'versions/{}'.format(n)
 
 
 def do_deploy(archive_path):
@@ -12,6 +39,19 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     else:
-        env.hosts = ['35.231.52.206', '54.226.104.83']
-        with Connection(env.hosts[0]) as c:
-            c.put("{}".format(archive_path), "/tmp/")
+        put("{}".format(archive_path), "/tmp/")
+        n = archive_path
+        n = n.replace("versions/", "")
+        n = n.replace(".tgz", "")
+        run('mkdir -p /data/web_static/releases/{}/'.format(n))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/\
+'.format(n, n))
+        run('rm /tmp/{}.tgz'.format(n))
+        run('mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/'.format(n, n))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(n))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current\
+'.format(n))
+        print("New version deployed!")
+        return True
