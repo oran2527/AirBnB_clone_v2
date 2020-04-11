813bc8de8c1ff8e7e63777a420f87a9739d92ebd
oran2527 <orlago250183@gmail.com>
2020-04-11 18:27:54 +0000

    point 0 reconstruction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,9 +2,9 @@
 # setup folders for deployment web static
 if ! which nginx > /dev/null 2>&1
 then
-   sudo apt-get update
-   sudo apt-get -y install nginx
-   sudo ufw allow 'Nginx HTTP'
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 fi
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
@@ -22,7 +22,27 @@ sudo echo "  </body>" | sudo tee -a /data/web_static/releases/test/index.html
 sudo echo "</html>" | sudo tee -a /data/web_static/releases/test/index.html
 if [[ -L '/data/web_static/current' ]]
 then
-   sudo rm /data/web_static/current
+sudo rm /data/web_static/current
 fi
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/*
+sudo service nginx stop
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /error_404.html;
+    location = /error_404.html {
+        root /usr/share/nginx/html;
+        internal;
+    }
+    add_header X-Served-By $HOSTNAME;
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+}" > /etc/nginx/sites-available/default
+sudo service nginx restart
