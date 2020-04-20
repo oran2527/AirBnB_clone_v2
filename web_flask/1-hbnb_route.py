a9fc6cd509cc7f064d7533b1ba6c91e9b0a98ab4
oran2527 <orlago250183@gmail.com>
2020-04-20 17:48:43 +0000

    point 1 hbnb pep8 file init

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' point 0 '''
+''' point 1 '''
 
 from flask import Flask
 app = Flask(__name__)
@@ -9,6 +9,12 @@ app = Flask(__name__)
 def index():
     '''index function'''
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''route hbnb function'''
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
